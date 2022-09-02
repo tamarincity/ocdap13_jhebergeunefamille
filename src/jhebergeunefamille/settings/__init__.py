a8cb4bb2d2e63f8b1ddbb345916089a41b06e8c8
PascalLeDeveloppeur <pascal.maitrel@hotmail.com>
2022-09-02 07:18:15 +0200

    fix settings of env variables

--- src/jhebergeunefamille/settings/__init__.py
+++ src/jhebergeunefamille/settings/__init__.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -19,6 +20,9 @@ env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,11 +87,11 @@ WSGI_APPLICATION = "jhebergeunefamille.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT')
+        'NAME': env('DB_NAME', default=""),
+        'USER': env('DB_USER', default=""),
+        'PASSWORD': env('DB_PASSWORD', default=""),
+        'HOST': env('DB_HOST', default=""),
+        'PORT': env('DB_PORT', default="")
     }
 }
 
